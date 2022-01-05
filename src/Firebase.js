df5b906d2f5ffe538e228f0b884f98f03fb3422a
Vision-Tey <nimusiimavision@gmail.com>
2022-01-05 18:18:47 +0300

    add movies

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJUZrfefYtTlFDl7MiLYAoRbCjWZeWsGM",
+    authDomain: "netflix-clone-316bb.firebaseapp.com",
+    projectId: "netflix-clone-316bb",
+    storageBucket: "netflix-clone-316bb.appspot.com",
+    messagingSenderId: "575128756959",
+    appId: "1:575128756959:web:2ae154c9f0273e38993f6a"
+  };
